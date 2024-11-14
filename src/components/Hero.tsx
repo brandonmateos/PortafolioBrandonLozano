25e2bd71fb8624c06cd3ec5bb72026cefdedb5d2
brandonmateos <blozano0@ucol.mx>
2024-11-14 08:25:27 -0600

    Commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
             Contáctame
           </a>
           <a
-            href="https://github.com/brandonmateos/PortafolioBrandonLozano/blob/main/cv.pdf"
+            href="./cv.pdf"
             className="bg-gray-800 hover:bg-gray-700 text-white px-8 py-3 rounded-full transition-colors"
             download
           >
